becdb68a96d017b107b101f543fc14886620fde6
Emil Godtfredsen <eg@arbitcds.com>
2021-05-17 12:11:03 +0200

    Author interface added

---
+++ TECH3Demo.API/Repositories/IAuthorRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TECH3Demo.API.Repositories
+{
+    public interface IAuthorRepository
+    {
+
+    }
+}

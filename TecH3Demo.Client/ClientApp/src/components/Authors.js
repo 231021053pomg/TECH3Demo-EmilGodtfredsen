5c357e7b2f83197896a9c809b3bbfc61bfca2277
Emil Godtfredsen <eg@arbitcds.com>
2021-05-25 14:09:16 +0200

    Created react app, started implementing CRUD on authors

--- TecH3Demo.Client/ClientApp/src/components/Authors.js
+++ TecH3Demo.Client/ClientApp/src/components/Authors.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Utils from './Common/Utils';
-import Table from 'reactstrap/lib/Table';
+import { Button, Table, Jumbotron, Row, Col, Form } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
@@ -12,6 +12,8 @@ export class Authors extends Component {
     super(props);
     this.state = {
       authors: undefined,
+      firstName: '',
+      lastName: '',
     };
   }
 
@@ -24,7 +26,6 @@ export class Authors extends Component {
       url: 'https://localhost:5001/api/author',
       method: 'GET',
     }).then(response => {
-      console.log(response.data)
       this.setState({
         authors: response.data
       });
@@ -45,20 +46,21 @@ export class Authors extends Component {
         <Table className="table table-striped table-hover table-borderless rounded sortable">
           <thead className="thead-dark">
             <tr>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Books</th>
+              <th scope="col" className='text-nowrap'>First Name</th>
+              <th scope="col" className='text-nowrap'>Last Name</th>
+              <th scope="col" className='text-nowrap'>Books</th>
+              <th></th>
             </tr>
           </thead>
-          <tbody>
+          <tbody id="tableRow">
             {this.state.authors.map((author, i) => {
               return (
                 <tr key={i} className='font-weight-bold'>
                   <td>{author.firstName}</td>
                   <td>{author.lastName}</td>
-                  
+                  <td><Button variant='dark'><FontAwesomeIcon icon="book" fixedWidth /> Show Books</Button></td>
+                  <td><Button variant='danger'><FontAwesomeIcon icon='trash-alt' fixedWidth /> Delete Author</Button></td>
                 </tr>
-
               )
             })}
           </tbody>
@@ -68,12 +70,72 @@ export class Authors extends Component {
     }
   }
 
+  handleAddAuthor = () =>{
+    axios({
+      url: 'https://localhost:5001/api/author',
+      method: 'POST',
+      data: {
+        firstName: this.state.firstName,
+        lastName: this.state.lastName
+      }
+    }).then(response => {
+      console.log(response)
+    }).catch((error) => {
+      this.handleAlert(Utils.handleAxiosError(error), 'danger')
+    })
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+
+  }
+
   render() {
     return (
-      <div>
-        <h1>Authors</h1>
-        {this.renderAuthorTable()}
-      </div>
+      <>
+        <Row>
+          <Col>
+            <Jumbotron className="mb-3">
+              <h1 className="title">
+                <Row>
+                  <Col>
+                    Authors
+                <FontAwesomeIcon icon="feather-alt" fixedWidth />
+                  </Col>
+                </Row>
+
+              </h1>
+            </Jumbotron>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {this.renderAuthorTable()}
+          </Col>
+        </Row>
+
+        <Form>
+          <Form.Group controlId="formBasicFirstName">
+            <Form.Control
+              placeholder="Enter First Name"
+              name='firstName'
+              onChange={this.handleChange.bind(this)}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicLastName">
+            <Form.Control
+              placeholder="Enter Last Name"
+              name='lastName'
+              onChange={this.handleChange.bind(this)}
+            />
+          </Form.Group>
+
+          <Button onClick={() => this.handleAddAuthor()} variant='success'><FontAwesomeIcon icon='plus' fixedWidth /> Add Author </Button>
+
+        </Form>
+      </>
     );
   }
 }
